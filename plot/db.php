e14501b259c2ca88450aac6ea8e01f1e9cf50d35
SAI SAMHITH REDDY <147680134+iamsaisamhithreddy@users.noreply.github.com>
2026-05-30 18:58:48 +0530

    Updated
    Updated database and Telegram Bot configuration values to placeholders.

--- plot/db.php
+++ plot/db.php
@@ -2,13 +2,13 @@
 
 // Database configuration
 $servername = "localhost";        
-$username   = "sairedd1_sai";     
-$password   = "saireddyA1@#$";    
-$dbname     = "sairedd1_trading"; 
+$username   = "";     
+$password   = "";    
+$dbname     = ""; 
 
 // Telegram Bot configuration
-$botToken    = "8065156652:AAFoy3EhYGlaHuFGFk_prHUzAvQFrz48Uus"; 
-$adminChatId = "655708526"; // optional
+$botToken    = "TELEGRAM BOT TOJEN"; 
+$adminChatId = "ADMIN CHAT ID"; // optional
 
 // Create database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
