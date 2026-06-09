cdafc47ff977269c893bf0135e02cbc3546bfb46
SAI SAMHITH REDDY <147680134+iamsaisamhithreddy@users.noreply.github.com>
2026-06-09 10:32:49 +0530

    Update trades_status_verify.php

--- livechart/trades_status_verify.php
+++ livechart/trades_status_verify.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__ . '/db.php'; 
+require_once __DIR__ . '/../db.php'; 
 
 // ==========================================
 // 1. FOREXFACTORY API FUNCTIONS
